c03069a60e8864358461a5bfafe00b9d5b4be322
Abbey <abigail.hartvigsen@gmail.com>
2019-02-21 11:09:12 -0700

    adding comments to go back and check my work against my teacher's

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { films } from './films.js'
 
 let foo = document.querySelector('.title')
 let bar= document.querySelector('.crawl')
-
+//check both above lines
 titleElement.textContent = films[0].title
 crawlElement.textContent = films[0].opening_crawl
+//check both above lines
