360ee6bfbb800bacc55e09144bf1e77ed88f3fa8
Abbey <abigail.hartvigsen@gmail.com>
2019-02-21 11:00:02 -0700

    Adding Star Wars data

---
+++ main.js
@@ -0,0 +1,8 @@
+import { films } from './films.js'
+
+
+let foo = document.querySelector('.title')
+let bar= document.querySelector('.crawl')
+
+titleElement.textContent = films[0].title
+crawlElement.textContent = films[0].opening_crawl
