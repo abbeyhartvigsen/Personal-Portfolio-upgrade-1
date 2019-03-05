fc623ad09be6eb72e4e6ec21951f0f12dcba06c6
Abbey <abigail.hartvigsen@gmail.com>
2019-03-05 12:44:01 -0700

    Added code from thortekexamplesmarch52019

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 import { films } from './films.js'
 
+const intro = document.querySelector('.intro')
 
-let foo = document.querySelector('.title')
-let bar= document.querySelector('.crawl')
-//check both above lines
-titleElement.textContent = films[0].title
-crawlElement.textContent = films[0].opening_crawl
-//check both above lines
+films.sort((a,b) => (a.episode_id > b.episode_id) ? 1 : -1 )
+
+films.forEach(film => {
+    let tile = document.createElement('div')
+
+    let titleElement = document.createElement('h1')
+  tile.appendChild(titleElement)
+  titleElement.textContent = film.title
+
+  let crawlElement = document.createElement('div')
+  tile.appendChild(crawlElement)
+  crawlElement.textContent = film.opening_crawl
+
+  intro.appendChild(tile)
+// let foo = document.querySelector('.title')
+// let bar= document.querySelector('.crawl')
+// //check both above lines
+// titleElement.textContent = films[0].title
+// crawlElement.textContent = films[0].opening_crawl
+// //check both above lines
