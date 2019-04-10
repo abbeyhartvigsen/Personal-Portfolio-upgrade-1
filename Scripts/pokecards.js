61dcb731347d0a282909a713deda70fe9efab884
Abbey <abigail.hartvigsen@gmail.com>
2019-04-09 20:33:24 -0600

    refactored the createPokeCard function

--- Scripts/pokecards.js
+++ Scripts/pokecards.js
@@ -1,6 +1,23 @@
 import { pokemon } from '../data/pokemon.js'
 
-pokemon.forEach((singleMon) => {
+console.log(pokemon)
+
+const mainContainer = document.querySelector('.container')
+
+function createPokeCard(pokeData) {
+    let card = document.createElement('div')
+    let name = document.createElement('h2')
+    let image = document.create('img')
+
+    let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1
+    title.textContent = upperName
+    image.src = pokeData.sprites.front_shiny
+    card.appendChild(name)
+    card.appendChild(image)
+    mainContainer.appendChild(card
+
+
+pokemon.forEach(singleMon => {
     fetch (singleMon.url)
     .then(function(response) {
         return response.json();
@@ -14,18 +31,18 @@ pokemon.forEach((singleMon) => {
 
 
 
-console.log(pokemon)
+// console.log(pokemon)
 
-const mainContainer = document.querySelector('.container')
+// const mainContainer = document.querySelector('.container')
 
-function createPokeCard(pokedata) {
-    let card = document.createElement('div')
-    let name = document.createElement('p')
-    let image = document.create('img')
+// function createPokeCard(pokedata) {
+//     let card = document.createElement('div')
+//     let name = document.createElement('p')
+//     let image = document.create('img')
 
     name.textContent = pokedata.name
-    image.src = pokedata.sprites.front_default
-    card.appendChild(name)
-    card.appendChild(image)
-    mainContainer.appendChild(card)
+    // image.src = pokedata.sprites.front_default
+    // card.appendChild(name)
+    // card.appendChild(image)
+    // mainContainer.appendChild(card)
 }
